c540d2c55f940f8603a606fe5f3304ecdbabcb0b
Joe Axel <jossefk1@gmail.com>
2023-06-10 19:22:22 +0300

    Preparation for deployment on axelrod.co.il

--- react-app/src/components/pages/ChatPage.tsx
+++ react-app/src/components/pages/ChatPage.tsx
@@ -3,7 +3,7 @@ import { TextField, Button, Grid, Typography, Switch, CircularProgress, Box } fr
 import io, { Socket } from 'socket.io-client';
 
 
-const SOCKET_SERVER_URL = process.env.REACT_APP_SOCKET_SERVER_URL || "http://localhost:3001";
+const SOCKET_SERVER_URL = (process.env.REACT_APP_SOCKET_SERVER_URL || "http://localhost:3001/api");
 
 enum UserType {
     HUMAN = 'human',
